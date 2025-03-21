0a8f5814209ee197bbe41c8fc910ff0dcef11212
Beture20 <femigbajuola@gmail.com>
2025-03-21 16:09:11 +0100

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-const transporter = require('../config/mailer');
+const transporter = require("../config/mailer");
 
-router.post('/send-email', async (req, res) => {
-    const { name, email, phone, message } = req.body;
-    
-    const contentHtml = `
+router.post("/send-email", async (req, res) => {
+  const { name, email, phone, message } = req.body;
+
+  const contentHtml = `
         <h1>User information</h1>
         <ul>
             <li>User: ${name}</li>
@@ -14,17 +14,22 @@ router.post('/send-email', async (req, res) => {
             <li>Phone: ${phone}</li>
         </ul>
         <p>Message: ${message}</p>
-    `
+    `;
 
+  try {
     const info = await transporter.sendMail({
-        from: '"Secreatary Mail" <nodemailer@sent.com>', //send address
-        to: 'example@domain.com', // list of receviers
-        subject: 'Email from Nodemailer',
-        html: contentHtml 
-    })
-
-    console.log(`Message sent ${info.messageId}`);
-    res.redirect('success.html')
+      from: '"Secreatary Mail" <nodemailer@sent.com>', //send address
+      to: "example@domain.com", // list of receviers
+      subject: "Get in touch with us",
+      html: contentHtml,
+    });
+    return res.status(200).json({ message: "Email sent successfully", info });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+  //   console.log(`Message sent ${info.messageId}`);
+  //   return res.status(200).json({ message: "Email sent successfully", info });
+  // res.redirect("success.html");
 });
 
 module.exports = router;
