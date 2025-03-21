42c60fffaeba908bc5726cc0027ddc6f8ce373fe
Beture20 <femigbajuola@gmail.com>
2025-03-21 16:21:46 +0100

    update here

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
 
 const app = express();
-
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(require('./routes/index'));
+app.use(require("./routes/index"));
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
+});
 
-app.listen(process.env.PORT_SERVER, err => {
-    if(err) throw err;
-    console.log(`Server running on port ${process.env.PORT_SERVER}`);
-})
+// app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(process.env.PORT_SERVER, (err) => {
+  if (err) throw err;
+  console.log(`Server running on port ${process.env.PORT_SERVER}`);
+});
